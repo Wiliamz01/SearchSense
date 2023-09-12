5f253d6c40d2b2e28295ada45553c46ee2fcfe74
Zhuokai Zhao <zhuokai@uchicago.edu>
2023-09-11 20:29:25 -0500

    Added grammar check using huggingface CoLA model.

---
+++ setup_env.sh
@@ -0,0 +1,8 @@
+# set up environment dependencies for the project
+pip install bs4
+pip install nltk
+pip install -U scikit-learn
+pip install pandas
+pip install zenrows
+pip install transformers
+pip install xformers
