ff902502f78892ead55b1202effae4890d1bb8cd
Carlos <carlos01oscar@berkeley.edu>
2023-10-02 16:59:57 -0700

    WORKS ON GCP

--- flaskdemo/app.py
+++ flaskdemo/app.py
@@ -5,3 +5,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return app.send_static_file('favicon.ico')
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return "500 error: " + str(error), 500
